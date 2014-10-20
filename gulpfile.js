c6424aed07b6cfd950a27029c214e8043731d849
Ken-Håvard Lieng <kk_998@hotmail.com>
2014-10-20 19:50:18 +0200

    Added autoprefixer, removed build output

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var minifyHTML = require('gulp-minify-html');
 var minifyCSS = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
 var uglify = require('gulp-uglify');
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
@@ -14,6 +15,7 @@ gulp.task('html', function() {
 
 gulp.task('css', function() {
     gulp.src('./src/*.css')
+        .pipe(autoprefixer())
         .pipe(minifyCSS())
         .pipe(gulp.dest('./dist'));
 });
@@ -23,8 +25,13 @@ gulp.task('js', function() {
         .bundle()
         .pipe(source('script.js'))
         .pipe(streamify(uglify()))
-        .pipe(gulp.dest('./dist'))
+        .pipe(gulp.dest('./dist'));
 });
+
+gulp.task('img', function() {
+    gulp.src('./src/img/*')
+        .pipe(gulp.dest('./dist/img'));
+})
 
 gulp.task('watch', ['default'], function() {
     gulp.watch('./src/*.html', ['html']);
@@ -32,4 +39,4 @@ gulp.task('watch', ['default'], function() {
     gulp.watch('./src/*.js', ['js']);
 });
 
-gulp.task('default', ['html', 'css', 'js']);
+gulp.task('default', ['html', 'css', 'js', 'img']);
